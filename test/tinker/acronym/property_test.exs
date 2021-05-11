1854624765b49251e140a0dcbe6dc2ce10b04c8b
troy.martin <troy.martin@riskrecon.com>
2021-05-11 14:20:02 -0400

    Update ntoes

--- test/tinker/acronym/property_test.exs
+++ test/tinker/acronym/property_test.exs
@@ -8,14 +8,9 @@ defmodule Tinker.Acronym.PropertyTest do
 
   @string_min 3
   @string_max 20
-  @runs 100_000
-
-  defp generate_string do
-    string(:ascii, min_length: @string_min, max_length: @string_max)
-  end
+  @runs 5
 
   # mix test.watch --only property
-  # https://hexdocs.pm/stream_data/ExUnitProperties.html#module-shrinking
   #
   # Example of a Factory
   property "works with many different phrases" do
@@ -27,24 +22,29 @@ defmodule Tinker.Acronym.PropertyTest do
             min_runs: @runs,
             max_runs: @runs
           ) do
-      # implements contract
+      # 1. conforms to contract
       assert {:ok, result} = abbreviate(phrase)
-      # should be a string
+      # 2. should be a string
       assert String.valid?(result)
-      # should all be uppercase
+      # 3. should all be uppercase
       assert result == String.upcase(result)
-      # should not contain spaces
+      # 4. should not contain spaces
       assert result == String.trim(result)
-      # should only contain A-Z
+      # 5. should only contain A-Z
       assert result
              |> String.to_charlist()
              |> Enum.all?(fn codepoint ->
                codepoint >= ?A and codepoint <= ?Z
              end)
 
-      Logger.warn("========================================")
-      Logger.warn("phrase: #{phrase}")
-      Logger.warn("result: #{result}")
+      # https://hexdocs.pm/stream_data/ExUnitProperties.html#module-shrinking
+      # Logger.warn("========================================")
+      # Logger.warn("phrase: #{phrase}")
+      # Logger.warn("result: #{result}")
     end
+  end
+
+  defp generate_string do
+    string(:ascii, min_length: @string_min, max_length: @string_max)
   end
 end
